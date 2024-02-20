06fd56200d801b2725edf523e52a452cb00c62e9
AhmedMostafa-147 <ahmad_hatem_2004@icloud.com>
2024-02-20 14:31:59 -0800

    ad

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -85,7 +85,7 @@
   };
 
   onMount(() => {
-    d3.csv("main/src/DSC106_NBA.csv").then((csvData) => {
+    d3.csv("data/DSC106_NBA.csv").then((csvData) => {
       data = csvData;
       renderBarChart(year);
     });
